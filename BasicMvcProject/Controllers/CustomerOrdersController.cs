1db1b9f79dc008cd4f42f4faf01bf06b1fbbf210
canbe <canbe@CANBERK>
2024-09-12 00:01:11 +0300

    Add project files.

---
+++ BasicMvcProject/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,38 @@
+﻿using BasicMvcProject.Models;
+using BasicMvcProject.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicMvcProject.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Canberk",
+                LastName = "Doğan",
+                Email = "candogan@patika.com"
+            };
+
+            var orders = new List<Order>();
+            {
+                orders.Add(new Order { Id = 1, Price = 123, ProductName = "babara", Quantity = 1 });
+                orders.Add(new Order { Id = 2, Price = 456, ProductName = "Ratatuy",Quantity = 1 });
+                
+            }
+
+
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+
+
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ BasicMvcProject/ViewModel/CustomerOrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BasicMvcProject.Models;
+
+namespace BasicMvcProject.ViewModel
+{
+    public class CustomerOrderViewModel
+    {
+        public Customer Customer { get; set; }
+
+        public List<Order> Orders { get; set; }
+
+    }
+}
